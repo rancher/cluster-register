d0902a753b175b1d617576f0ca60ad1cfbf30440
Josh Curl <josh@curl.me>
2017-08-16 14:25:39 -0700

    Initial commit

--- main.go
+++ main.go
@@ -1,23 +1,109 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"io/ioutil"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/urfave/cli"
+	"k8s.io/client-go/rest"
+	"github.com/rancher/go-rancher/v3"
 )
 
-var VERSION = "v0.0.0-dev"
+const (
+	rancherCredentialsFolder = "/rancher-credentials"
+	urlKeyFilename = "url"
+	accessKeyFilename = "access-key"
+	secretKeyFilename = "secret-key"
+
+	kubernetesServiceHostKey = "KUBERNETES_SERVICE_HOST"
+	kubernetesServicePortKey = "KUBERNETES_SERVICE_PORT"
+)
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "credential-reporter"
-	app.Version = VERSION
-	app.Usage = "You need help!"
-	app.Action = func(c *cli.Context) error {
-		logrus.Info("I'm a turkey")
-		return nil
+	if err := runReporter(); err != nil {
+		panic(err)
+	}
+}
+
+func runReporter() error {
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		return err
 	}
 
-	app.Run(os.Args)
+	if err := populateCAData(cfg); err != nil {
+		return err
+	}
+
+	kubernetesServiceHost, err := getenv(kubernetesServiceHostKey)
+	if err != nil {
+		return err
+	}
+	kubernetesServicePort, err := getenv(kubernetesServicePortKey)
+	if err != nil {
+		return err
+	}
+
+	rancherClient, err := getRancherClient()
+	if err != nil {
+		return err
+	}
+
+	_, err = rancherClient.Cluster.Create(&client.Cluster{
+		// TODO
+		Name: "josh",
+		Embedded: false,
+		K8sClientConfig: client.K8sClientConfig{
+			Address: fmt.Sprintf("%s:%s", kubernetesServiceHost, kubernetesServicePort),
+			BearerToken: cfg.BearerToken,
+			CaCert: string(cfg.CAData),
+		},
+	})
+	return err
+}
+
+func getenv(env string) (string, error) {
+	value := os.Getenv(env)
+	if value == "" {
+		return "", fmt.Errorf("%s is empty", env)
+	}
+	return value, nil
+}
+
+func populateCAData(cfg *rest.Config) error {
+	bytes, err := ioutil.ReadFile(cfg.CAFile)
+	if err != nil {
+		return err
+	}
+	cfg.CAData = bytes
+	return nil
+}
+
+func getRancherClient() (*client.RancherClient, error) {
+	url, err := readKey(urlKeyFilename)
+	if err != nil {
+		return nil, err
+	}
+	accessKey, err := readKey(accessKeyFilename)
+	if err != nil {
+		return nil, err
+	}
+	secretKey, err := readKey(secretKeyFilename)
+	if err != nil {
+		return nil, err
+	}
+	return client.NewRancherClient(&client.ClientOpts{
+		Url: url,
+		AccessKey: accessKey,
+		SecretKey: secretKey,
+	})
+}
+
+func readKey(key string) (string, error){
+	bytes, err := ioutil.ReadFile(path.Join(rancherCredentialsFolder, key))
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
 }
